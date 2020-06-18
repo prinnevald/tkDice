0389f924e09ad325c0543050316b3202e255b8f9
Dnislam Urazayev <52580585+prinnevald@users.noreply.github.com>
2020-06-19 01:09:01 +0600

    Add files via upload

--- tkDice.py
+++ tkDice.py
@@ -6,37 +6,37 @@ import random
 # sides from 1 through 6.
 
 def draw1(canvas):
-    canvas.create_oval(90, 90, 120, 120, fill="#1f1")
+    canvas.create_oval(90, 90, 120, 120, fill="black")
 
 def draw2(canvas):
-    canvas.create_oval(50, 50, 70, 70, fill="#1f1")
-    canvas.create_oval(130, 130, 150, 150, fill="#1f1")
+    canvas.create_oval(50, 50, 70, 70, fill="black")
+    canvas.create_oval(130, 130, 150, 150, fill="black")
 
 def draw3(canvas):
-    canvas.create_oval(50, 50, 70, 70, fill="#1f1")
-    canvas.create_oval(130, 130, 150, 150, fill="#1f1")
-    canvas.create_oval(90, 90, 120, 120, fill="#1f1")
+    canvas.create_oval(50, 50, 70, 70, fill="black")
+    canvas.create_oval(130, 130, 150, 150, fill="black")
+    canvas.create_oval(90, 90, 120, 120, fill="black")
 
 def draw4(canvas):
-    canvas.create_oval(50, 50, 70, 70, fill="#1f1")
-    canvas.create_oval(130, 130, 150, 150, fill="#1f1")
-    canvas.create_oval(130, 50, 150, 70, fill="#1f1")
-    canvas.create_oval(50, 130, 70, 150, fill="#1f1")
+    canvas.create_oval(50, 50, 70, 70, fill="black")
+    canvas.create_oval(130, 130, 150, 150, fill="black")
+    canvas.create_oval(130, 50, 150, 70, fill="black")
+    canvas.create_oval(50, 130, 70, 150, fill="black")
 
 def draw5(canvas):
-    canvas.create_oval(50, 50, 70, 70, fill="#1f1")
-    canvas.create_oval(130, 130, 150, 150, fill="#1f1")
-    canvas.create_oval(130, 50, 150, 70, fill="#1f1")
-    canvas.create_oval(50, 130, 70, 150, fill="#1f1")
-    canvas.create_oval(90, 90, 120, 120, fill="#1f1")
+    canvas.create_oval(50, 50, 70, 70, fill="black")
+    canvas.create_oval(130, 130, 150, 150, fill="black")
+    canvas.create_oval(130, 50, 150, 70, fill="black")
+    canvas.create_oval(50, 130, 70, 150, fill="black")
+    canvas.create_oval(90, 90, 120, 120, fill="black")
 
 def draw6(canvas):
-    canvas.create_oval(50, 50, 70, 70, fill="#1f1")
-    canvas.create_oval(130, 130, 150, 150, fill="#1f1")
-    canvas.create_oval(130, 50, 150, 70, fill="#1f1")
-    canvas.create_oval(50, 130, 70, 150, fill="#1f1")
-    canvas.create_oval(50, 90, 70, 110, fill="#1f1")
-    canvas.create_oval(130, 90, 150, 110, fill="#1f1")
+    canvas.create_oval(50, 50, 70, 70, fill="black")
+    canvas.create_oval(130, 130, 150, 150, fill="black")
+    canvas.create_oval(130, 50, 150, 70, fill="black")
+    canvas.create_oval(50, 130, 70, 150, fill="black")
+    canvas.create_oval(50, 90, 70, 110, fill="black")
+    canvas.create_oval(130, 90, 150, 110, fill="black")
 
 # list of drawing functions to get a function from randomly
 
@@ -55,7 +55,7 @@ class Dice(Frame):
         self.pack(fill=BOTH, expand=1)
         
         canvas = Canvas(self)
-        canvas.create_rectangle(10, 10, 210, 210)
+        canvas.create_rectangle(10, 10, 210, 210, fill="white")
         
         random.choice(drawList)(canvas)
         
